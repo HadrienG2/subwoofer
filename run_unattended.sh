9dfe69ecb5e3323ecc2c94f73d5280ee1f863aff
Hadrien Grasland <hadrien.grasland@gmx.fr>
2024-12-28 16:04:58 +0100

    Cargo criterion compatibility hacks

--- run_unattended.sh
+++ run_unattended.sh
@@ -3,14 +3,14 @@
 # Optimized unattended execution configuration.
 #
 # This starts with the configuration that runs the quickest, then gradually
-# proceeds with longer-running tests that produce more detailed output. This
-# ensures, at the expense of a relatively small increase in overall running
+# proceeds with longer-running benchmarks that produce more detailed output.
+# This ensures, at the expense of a relatively small increase in overall running
 # time, that you can get maximally useful data for a given amount of benchmark
 # execution time during unattended execution.
 #
-# This was only tested on Linux and with x86 CPUs, and assumes availability of
-# cargo criterion, perf and classic utilities like bash, grep and lscpu. Patches
-# to extend hardware or OS support are welcome!
+# This script was only tested on Linux and with x86_64 CPUs. It assumes
+# availability of cargo criterion, perf and classic utilities like bash, grep
+# and lscpu. Patches to extend hardware or OS support are welcome!
 
 ### START FROM A CLEAN SLATE ###
 
@@ -37,44 +37,55 @@ echo '=== If no operation is slowed down by subnormals, you can stop here ==='
 # barriers that we need to apply to prevent autovectorization, and thus the
 # negative impact of such barriers on the quality of generated code.
 #
-# Parametrized by a command that behaves like `cargo bench` and accepts cargo
-# build/bench arguments, but may also produce perf.data files.
+# Parametrized by a command that behaves like `cargo bench` in the sense that it
+# accepts a `--bench` or `--benches` argument, but that command may also produce
+# perf.data files and they will be given a more explicit name as needed.
 function bench_each_type() {
+    # Give the perf.data, if any, a prefix dictated by the first argument
     function rename_perf() {
         if [[ -e perf.data ]]; then
             mv perf.data perf.data.$1
         fi
     }
+    # Benchmark a number of floating-point types and rename their perf.data
+    function bench_types() {
+        for type in $*; do
+            $* --bench=${type}
+            rename_perf opt.${type}
+        done
+    }
+    # Optimized codegen for x86
     if [[ $(lscpu | grep x86) ]]; then
+        # Detect FMA support
         FMA_FLAG='-C target-feature='
         if [[ $(lscpu | grep fma) ]]; then
             FMA_FLAG="${FMA_FLAG}+fma"
         else
             FMA_FLAG="${FMA_FLAG}-fma"
         fi
+        # Optimized AVX-512 codegen
+        #
+        # avx512vl is not really needed, but we did not yet implement support
+        # for type-dependent register counts because it is not yet needed on any
+        # real-world CPU. So we need avx512vl for the benchmark to allow itself
+        # to use all available 32 registers.
         if [[ $(lscpu | grep avx512vl) ]]; then
-            # avx512vl is not really needed, but we did not implement support
-            # for type-dependent register counts yet as it is not needed on any
-            # real-world CPU, so we need avx512vl for the benchmark to allow
-            # itself to use all available 32 registers.
-            RUSTFLAGS="${FMA_FLAG},+avx512f,+avx512vl" $* --bench=f32x16 --bench=f64x08
-            rename_perf opt.avx512
+            RUSTFLAGS="${FMA_FLAG},+avx512f,+avx512vl" bench_types f32x16 f64x08
         fi
+        # Optimized AVX codegen
         if [[ $(lscpu | grep avx) ]]; then
-            RUSTFLAGS="${FMA_FLAG},+avx" $* --bench=f32x08 --bench=f64x04
-            rename_perf opt.avx
+            RUSTFLAGS="${FMA_FLAG},+avx" bench_types f32x08 f64x04
         fi
-        # Should not activate FMA for <= SSE, as it also activates AVX and thus
-        # degrades codegen.
+        # No FMA for SSE/scalar because it also activates AVX, and thus hurts
+        # codegen as we need optimization barriers to avoid autovectorization
         if [[ $(lscpu | grep sse2) ]]; then
-            RUSTFLAGS='-C target-feature=+sse2' $* --bench=f32x04 --bench=f64x02
-            rename_perf opt.sse2
+            RUSTFLAGS='-C target-feature=+sse2' bench_types f32x04 f64x02
         fi
-        RUSTFLAGS='' $* --bench=f32 --bench=f64
-        rename_perf opt.scalar
+        RUSTFLAGS='' bench_types f32 f64
     else
+        # Unoptimized codegen for unknown CPUs
         if [[ -v WARNED_ABOUT_TARGET_FEATURES ]]; then
-            echo '--- WARNING: Unknown hardware architecture, target-features may be suboptimal ---'
+            echo '--- WARNING: Unknown hardware architecture, target features may be suboptimal ---'
             WARNED_ABOUT_TARGET_FEATURES=1
         fi
         $* --benches
@@ -85,7 +96,7 @@ function bench_each_type() {
 # Decide which flags must be applied to perf record on this CPU
 PERF_FLAGS=''
 if [[ $(lscpu | grep AMD) ]]; then
-    # perf record without -a is not reliably available on AMD chips...
+    # perf record without -a is not always available on AMD chips...
     PERF_FLAGS="${PERF_FLAGS} -a"
 fi
 
@@ -94,6 +105,8 @@ fi
 # Must be parametrized with cargo's bench selection build arguments, either
 # `--benches` to build all benchmarks or a sequence of `--bench=x --bench=y ...`
 # to only build a specific set of benchmarks.
+#
+# For now, we are using cargo criterion, which only accepts a single --bench.
 function check_codegen() {
     cargo build --profile=bench --features=codegen $*
     perf record ${PERF_FLAGS} -- cargo criterion --features=codegen $* -- --profile-time=1
